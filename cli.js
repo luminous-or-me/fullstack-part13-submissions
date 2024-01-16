f3f843209b175a37c59c34d1b9a28f109b67bdd7
Anwar Sh. <alvaro.sh03@gmail.com>
2024-01-16 21:06:19 +0500

    Change the structure of the app

--- cli.js
+++ cli.js
@@ -1,51 +1,17 @@
 require("dotenv").config();
 
-const { Sequelize, QueryTypes, Model, DataTypes } = require("sequelize");
-
-const sequelize = new Sequelize(process.env.DATABASE_URL);
-
-class Blog extends Model {}
-
-Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    author: {
-      type: DataTypes.TEXT,
-    },
-    url: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    title: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    likes: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    underscored: true,
-    modelName: "blog",
-  }
-);
+const { Blog } = require("./models");
+const { connectToDatabase } = require("./util/db");
 
 const main = async () => {
   try {
+    await connectToDatabase();
+
     const blogs = await Blog.findAll();
 
     blogs.forEach(({ author, title, likes }) => {
       console.log(`${author}: '${title}', ${likes} likes`);
     });
-
-    sequelize.close();
   } catch (error) {
     console.log("Unable to connect to the database:", error);
   }
---
+++ util/db.js
@@ -0,0 +1,22 @@
+const { Sequelize } = require("sequelize");
+
+const { DATABASE_URL } = require("./config");
+
+const sequelize = new Sequelize(DATABASE_URL);
+
+const connectToDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("connected to the database");
+  } catch (error) {
+    console.error("failed to connect to the database");
+    return process.exit(1);
+  }
+
+  return null;
+};
+
+module.exports = {
+  sequelize,
+  connectToDatabase,
+};
