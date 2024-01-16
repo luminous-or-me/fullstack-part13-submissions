16906a4f8aeddd6d1c8738ed47f8a6e78cd8e685
Anwar Sh. <alvaro.sh03@gmail.com>
2024-01-16 20:43:13 +0500

    Implement simple web application

--- index.js
+++ index.js
@@ -1,9 +1,73 @@
 require("dotenv").config();
 
+const { Sequelize, Model, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    underscored: true,
+    modelName: "blog",
+  }
+);
+
+Blog.sync();
+
 const express = require("express");
 const app = express();
 
 app.use(express.json());
+
+app.get("/api/blogs", async (req, res) => {
+  const blogs = await Blog.findAll();
+
+  return res.json(blogs);
+});
+
+app.post("/api/blogs", async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+
+    return res.status(201).json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  try {
+    await Blog.destroy({ where: { id: req.params.id } });
+    return res.status(204).end();
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
 
 const PORT = process.env.PORT || 3001;
 
