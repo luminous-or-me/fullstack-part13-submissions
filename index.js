b24b7b0658b615857218264cfeebec3495245364
Anwar Sh. <alvaro.sh03@gmail.com>
2024-01-16 21:37:22 +0500

    Implement error handling middleware

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const { PORT } = require("./util/config");
 const { connectToDatabase } = require("./util/db");
 const blogsRouter = require("./controllers/blogs");
 
+const { errorHandler } = require("./util/middleware");
+
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
+
+app.use(errorHandler);
 
 const start = async () => {
   await connectToDatabase();
---
+++ util/middleware.js
@@ -0,0 +1,15 @@
+const errorHandler = (error, req, res, next) => {
+  console.error(error);
+
+  if (error.name === "SequelizeValidationError") {
+    return res.status(400).json({ error: "invalid data" });
+  }
+
+  if (error.name === "SequelizeDatabaseError") {
+    return res.status(400).json({ error: "invalid data" });
+  }
+
+  next(error);
+};
+
+module.exports = { errorHandler };
