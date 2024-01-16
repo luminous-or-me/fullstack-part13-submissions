4acb76bf9f346aef8c970b5d50ff1300e0520980
Anwar Sh. <alvaro.sh03@gmail.com>
2024-01-16 21:27:29 +0500

    Implement PUT /api/blogs/:id endpoint

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 const { Blog } = require("../models");
 
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id);
+  next();
+};
+
 router.get("/", async (req, res) => {
   const blogs = await Blog.findAll();
 
@@ -17,12 +22,23 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.delete("/:id", async (req, res) => {
-  try {
-    await Blog.destroy({ where: { id: req.params.id } });
-    return res.status(204).end();
-  } catch (error) {
-    return res.status(400).json({ error });
+router.delete("/:id", blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy();
+  }
+
+  return res.status(204).end();
+});
+
+router.put("/:id", blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes;
+
+    await req.blog.save();
+
+    res.json(req.blog);
+  } else {
+    res.status(404).json({ error: "blog not found" });
   }
 });
 
